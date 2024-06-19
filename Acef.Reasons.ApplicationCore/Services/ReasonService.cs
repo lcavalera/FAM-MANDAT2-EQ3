3a183ee4e0c008bdea84fcf90cece4a1bb11aef2
Luca H23 Dev Info <2298949@cegeplimoilou.ca>
2024-06-19 12:28:44 -0400

    modif methode Edit

--- Acef.Reasons.ApplicationCore/Services/ReasonService.cs
+++ Acef.Reasons.ApplicationCore/Services/ReasonService.cs
@@ -80,13 +80,57 @@ namespace Acef.Reasons.ApplicationCore.Services
                     }
                 };
 
-            consultationReasonToEdit.Name = reason.Name;
-            consultationReasonToEdit.Description = reason.Description;
+            // Check if a consultation reason already exists
+            //var consultationReasons = await GetConsultationReasons();
+            var consultationReasons = await _reasonRepository.ListAsync();
 
-            _logger.LogInformation($"{DateTime.Now} [*] Modifying consultation reason " +
-                $"(id = {consultationReasonToEdit.ID})...");
+            if (consultationReasons.Any(c =>
+                    c.Name.ToLower() == reason.Name.ToLower() &&
+                    !c.IsDeleted &&
+                    c.ID != consultationReasonToEdit.ID))
+            {
+                throw new HttpException
+                {
+                    StatusCode = StatusCodes.Status400BadRequest,
+                    Errors = new
+                    {
+                        //Errors = $"{DateTime.Now} [-] Consultation reason already exists"
+                        Errors = $"Consultation reason already exists"
+                    }
+                };
+            }
 
-            await _reasonRepository.EditAsync(consultationReasonToEdit);
+            // Check if a deleted consultation reason have the same name as the one being modified
+            var deletedConsultationReasonWithSameName = consultationReasons.FirstOrDefault(
+                c => c.Name == reason.Name && c.IsDeleted);
+
+            // If a deleted one exists with the same name, we can reactivate the old one...
+            // ...and deactivate (delete) the new one
+
+            if (deletedConsultationReasonWithSameName != null)
+            {
+                deletedConsultationReasonWithSameName.IsDeleted = false;
+                deletedConsultationReasonWithSameName.DeletedAt = null;
+                deletedConsultationReasonWithSameName.Description = reason.Description;
+
+                _logger.LogInformation($"{DateTime.Now} [*] Modifying consultation reason " +
+                    $"(id = {deletedConsultationReasonWithSameName.ID})...");
+
+                await _reasonRepository.EditAsync(deletedConsultationReasonWithSameName);
+
+                await Delete(consultationReasonToEdit.ID);
+            }
+            else
+            {
+                consultationReasonToEdit.Name = reason.Name;
+                consultationReasonToEdit.Description = reason.Description;
+
+                _logger.LogInformation($"{DateTime.Now} [*] Modifying consultation reason " +
+                    $"(id = {consultationReasonToEdit.ID})...");
+
+                await _reasonRepository.EditAsync(consultationReasonToEdit);
+
+            }
         }
 
         public async Task<ReasonDTO> GetById(int id)
@@ -124,7 +168,7 @@ namespace Acef.Reasons.ApplicationCore.Services
         public async Task<IEnumerable<ReasonDTO>> Get()
         {
             var consultationReasons = await _reasonRepository.ListAsync();
-            
+
             _logger.LogInformation($"{DateTime.Now} [*] Getting ${consultationReasons.Count()} " +
                 $"consultation reason(s)...");
 
