72ee3f242530185d2525f0b0c40ed82ba22ec9e2
Luca H23 Dev Info <2298949@cegeplimoilou.ca>
2024-06-19 10:44:21 -0400

    Add RaisonLog

---
+++ Acef.MVC/Models/Log/RaisonLog.cs
@@ -0,0 +1,10 @@
+﻿namespace Acef.MVC.Models.Log
+{
+    public class RaisonLog
+    {
+        public const int Lecture = 100;
+        public const int Creation = 101;
+        public const int Modification = 102;
+        public const int Suppression = 103;
+    }
+}
